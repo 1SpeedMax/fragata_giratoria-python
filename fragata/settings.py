63c85b12f3bfbaa64de77584d17b56cd3a408398
Tu Nombre <ajlfsaljfasjf@gmail.com>
2026-05-30 15:54:55 -0500

    Configurar PostgreSQL

--- fragata/settings.py
+++ fragata/settings.py
@@ -162,14 +162,7 @@ if DATABASE_URL:
             ssl_require=True
         )
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
-    
+
 # ======================
 # INTERNACIONALIZACIÓN
 # ======================
